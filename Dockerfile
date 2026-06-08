a4683120507cb85bb8556b809c67598c29f62b42
Srikkanth Muruganandam <srikkanth@teknoturf.com>
2026-06-08 16:20:33 +0530

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+# Use latest Python base image
+FROM python:latest
+
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy requirements.txt and install dependencies
+COPY requirements.txt ./
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the rest of the application code
+COPY ./app ./app
+
+# Expose the port uvicorn will run on
+EXPOSE 8001
+
+# Run the FastAPI app with Uvicorn
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8001"]
---
+++ app/main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="app/templates")
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post("/calculate")
+def calculate(
+        request: Request,
+        principal: float = Form(...),
+        rate: float = Form(...),
+        time: float = Form(...)
+    ):
+
+    interest = (principal * rate * time) / 100
+
+    return templates.TemplateResponse(
+        "result.html",
+        {
+            "request": request,
+            "principal": principal,
+            "rate": rate,
+            "time": time,
+            "interest": interest
+        }
+    )
